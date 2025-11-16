ea36355e44c56759d8c716639d4c00fb8dc26dff
Suthashan Tharmarajah <su-n@mac.home>
2025-11-16 18:45:29 -0500

    Fix homepage route to match test expectation

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
-    return "test func"
+    return "Hello, DevOps World!"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
